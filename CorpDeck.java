4ef3e219dc421a9c3f783356c59263f6a66a5204
DSV-Destel <kevin.niklasson@gmail.com>
2015-10-13 13:42:50 +0200

    Fork -> Branch
    Woops, jag gjorde bort mig. Sorry! Satt och uppdaterade en fork, här är
    min kod

---
+++ CorpDeck.java
@@ -0,0 +1,23 @@
+public class CorpDeck extends Deck {
+
+	public CorpDeck(String side) {
+		super();
+		side = "corp";
+	}
+	protected void addCard(Card[] card, int index){
+		/*if(card[index].getFactionLetter() == "h" || 
+			card[index].getFactionLetter() == "j" || 
+			card[index].getFactionLetter() == "w" || 
+			card[index].getFactionLetter() == "n"){*/
+			super.getDeckOfCards().add(card);	
+		//}	
+	}
+	
+	protected int getSize(){
+		return super.getDeckOfCards().size();
+	}
+
+	
+	
+	
+}
