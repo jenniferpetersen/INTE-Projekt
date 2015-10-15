cc967b5c95ecf75793f8c7e1a4b0d847d9352bdd
DSV-Destel <kevin.niklasson@gmail.com>
2015-10-15 11:15:02 +0200

    Fiskinvestment + Runner changes
    Lade till addFiskInvestmentCardToStack() bara för att kunna ha en stack
    att testa med, får tas bort senare.  Lade till tester till kortet Fisk
    Investment Seminar, allt funkar än så länge.

--- Runner.java
+++ Runner.java
@@ -1,4 +1,5 @@
 package netrunner;
+
 import java.util.ArrayList; 
 
 class Runner extends Player{
@@ -11,12 +12,27 @@ class Runner extends Player{
 		super(4); 
 	}
 	
+	public void addFiskInvestmentCardToStack(){
+		Card card1 = new Card("Fisk Investment Seminar", "event", "Priority", "Play only as your first [Click].\r\nEach player draws 3 cards.", 0, "Criminal", "criminal", "c", "2",
+				false);
+		stack.add(card1);
+	}
+	
 	public Card getTopCardFromStack(){
 		return stack.get(0); 
 	}
 	
+	public boolean isStackEmpty() {
+		return stack.isEmpty();
+	}
+	
 	public void drawCard(){
-		
+		if(stack.size() > 0) {
+			grip.add(getTopCardFromStack());
+			stack.remove(getTopCardFromStack());
+		}else{
+			
+		}
 	}
 
 
