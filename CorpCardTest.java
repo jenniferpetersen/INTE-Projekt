3991e3c44b248554419799a4b73753a703d13510
DSV-Destel <kevin.niklasson@gmail.com>
2015-10-16 14:50:45 +0200

    CorpCard
    100% täckning för de fyra metoderna

--- CorpCardTest.java
+++ CorpCardTest.java
@@ -19,13 +19,39 @@ public class CorpCardTest {
 		corp.addAdvancementTokens(-1);
 	}
 	
-	@Test
-	public void testRemoveAdvancementTokens(){
-		
+	@Test(expected = java.lang.IllegalArgumentException.class)
+	public void testAddNegativeAdvancementToken(){
+		CorpCard corp = new CorpCard("Hedgefund");
+		corp.addAdvancementTokens(-1);
 	}
 	
 	@Test
-	public void testRemoveAdvancementTokensNegative(){
-		
+	public void testRemoveAdvancementTokens(){
+		CorpCard corp = new CorpCard("Hedgefund");
+		corp.addAdvancementTokens(5);
+		corp.removeAdvancementTokens(3);
+		assertEquals(2, corp.getAdvancementTokens());
 	}
+	
+	@Test(expected = java.lang.IllegalArgumentException.class)
+	public void testRemoveNegativeAdvancementTokens(){
+		CorpCard corp = new CorpCard("Hedgefund");
+		corp.removeAdvancementTokens(-1);
+	}
+	
+	@Test(expected = java.lang.IllegalArgumentException.class)
+	public void testRemoveAdvancementTokensNegative(){
+		CorpCard corp = new CorpCard("Hedgefund");
+		corp.addAdvancementTokens(0);
+		corp.removeAdvancementTokens(1);
+	}
+	
+	@Test
+	public void testBuyAdvancementTokens(){
+		Corporation corp = new Corporation();
+		CorpCard corpcard = new CorpCard("Hedgefund");
+		corpcard.buyAdvancementTokens(corp);
+		assertEquals(4, corp.getCredits());
+	}
+	
 }
