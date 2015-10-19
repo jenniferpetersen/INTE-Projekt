5d64615837161e235d2c84674a8d4f6bfe0f28e3
DSV-Destel <kevin.niklasson@gmail.com>
2015-10-19 12:50:50 +0200

    Deck & DeckTest
    Städade upp koden, gjorde om en del eftersom det mesta var utdaterat.

--- RunnerDeck.java
+++ RunnerDeck.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 abstract class RunnerDeck extends Deck {
 
-	public RunnerDeck(ArrayList<Card[]> deckOfCards, String side) {
+	public RunnerDeck(ArrayList<Card> deckOfCards, String side) {
 		super();
 		side = "runner";
 		// TODO Auto-generated constructor stub
 	}
-	protected void addCard(Card[] runnerCard){
+	protected void addCard(Card runnerCard){
 		// TODO addcard
 	}
 }
