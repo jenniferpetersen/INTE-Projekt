2c58161f0eae39864d8550a0e607f42e9eb34214
DSV-Mash <rania.95705mash@gmail.com>
2015-10-19 13:45:43 +0200

    Update Hardware.java

--- Hardware.java
+++ Hardware.java
@@ -2,11 +2,11 @@ package netrunner;
 
 public class Hardware extends RunnerCard {
 	
-	public Hardware(String title, String subTypeCode, String text, int cost, 
+	public Hardware(String title, String subTypeCode, String text, int cost,
 			String factionCode, String factionCost,
 			boolean uniqueness){
 		
-		super(title, "hardware", subTypeCode, text, cost, factionCode, factionCost, uniqueness);
+		super(title, "hardware", subTypeCode, text, cost, factionCode,  factionCost, uniqueness);
 	}
 
 }
