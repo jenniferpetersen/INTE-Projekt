34e6afbdfe870be5dac86622d204bb3213753ae7
Jennifer Petersen <jenniferpetersen089@gmail.com>
2015-10-19 12:20:44 +0200

    Fler tester för Cerebral_Imagining om att lägga till/ta bort negativa credits

--- Cerebral_Imagining.java
+++ Cerebral_Imagining.java
@@ -10,5 +10,9 @@ public class Cerebral_Imagining extends Corporation{
 		super.gainCredits(change);
 		setMaxHandSize(getCredits());
 	}
-
+	
+	public void loseCredits(int change) {
+		super.loseCredits(change);
+		setMaxHandSize(getCredits());
+	}
 }
--- Cerebral_ImaginingTest.java
+++ Cerebral_ImaginingTest.java
@@ -7,10 +7,28 @@ import org.junit.Test;
 public class Cerebral_ImaginingTest {
 
 	@Test
-	public void addCreditsHandSizeTest() {
+	public void gainCreditsHandSizeTest() {
 		Cerebral_Imagining CI = new Cerebral_Imagining();
 		CI.gainCredits(1);
 		assertEquals(6, CI.getMaxHandSize());
 	}
+	
+	@Test
+	public void loseCreditsHandSizeTest() {
+		Cerebral_Imagining CI = new Cerebral_Imagining();
+		CI.loseCredits(1);
+		assertEquals(4, CI.getMaxHandSize());
+	}
 
+	@Test (expected = java.lang.IllegalArgumentException.class)
+	public void gainNegCreditsHandSizeTest() {
+		Cerebral_Imagining CI = new Cerebral_Imagining();
+		CI.gainCredits(-1);
+	}
+	
+	@Test (expected = java.lang.IllegalArgumentException.class)
+	public void loseNegCreditsHandSizeTest() {
+		Cerebral_Imagining CI = new Cerebral_Imagining();
+		CI.loseCredits(-1);
+	}
 }
