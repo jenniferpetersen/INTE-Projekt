5043a19c7a4a02f773f104cee4d349ed1e2f0952
DSV-Mash <rania.95705mash@gmail.com>
2015-10-19 13:24:55 +0200

    Update Event.java

--- Event.java
+++ Event.java
@@ -2,7 +2,7 @@
 public class Event extends RunnerCard{
 
 	
-	public Event(String title, String typecode, String subTypeCode, String text, int cost, String faction,
+	public Event(String title, String subTypeCode, String text, int cost, String faction,
 			String factionCode, String factionletter, String factionCost,
 			boolean uniqueness) {
 		
