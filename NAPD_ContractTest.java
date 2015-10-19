62464291074bc03eeb3fa6d19b1da71125a696c5
Jennifer Petersen <jenniferpetersen089@gmail.com>
2015-10-19 16:05:16 +0200

    Fler tester till NAPD, kollar så kort tas bort från där de accessats vid steal, samt att den är tillräckligt advancead för att scoreas för corp

--- NAPD_ContractTest.java
+++ NAPD_ContractTest.java
@@ -11,9 +11,23 @@ public class NAPD_ContractTest {
 		Runner runner = new Runner();
 		Cerebral_Imaging corp = new Cerebral_Imaging();
 		NAPD_Contract napd = new NAPD_Contract();
+		corp.addCardToRD(napd);
 		
-		napd.stealAgenda(corp, runner, "Archives");
+		napd.stealAgenda(corp, runner, "researchAndDevelopment");
 		assertEquals(2, runner.getAgendaPoints());
+		assertEquals(1, runner.getCredits());
+	}
+	
+	@Test (expected = java.lang.IllegalArgumentException.class)
+	public void stealTooFewCreditsTest() {
+		Runner runner = new Runner();
+		Cerebral_Imaging corp = new Cerebral_Imaging();
+		NAPD_Contract napd = new NAPD_Contract();
+		corp.addCardToRD(napd);
+		
+		runner.loseCredits(2);
+		
+		napd.stealAgenda(corp, runner, "researchAndDevelopment");
 	}
 	
 	@Test
@@ -21,8 +35,54 @@ public class NAPD_ContractTest {
 		Cerebral_Imaging corp = new Cerebral_Imaging();
 		NAPD_Contract napd = new NAPD_Contract();
 		
-		napd.scoreAgenda(corp, "Archives");
+		napd.advance();
+		napd.advance();
+		napd.advance();
+		napd.advance();
+		
+		napd.scoreAgenda(corp);
 		assertEquals(2, corp.getAgendaPoints());
+	}
+	
+	@Test(expected = java.lang.IllegalArgumentException.class)
+	public void scoreTooFewAdvTokensTest() {
+		Cerebral_Imaging corp = new Cerebral_Imaging();
+		NAPD_Contract napd = new NAPD_Contract();
+		
+		napd.scoreAgenda(corp);
+	}
+	
+	@Test
+	public void testRemoveFromRDAfterSteal() {
+		Cerebral_Imaging corp = new Cerebral_Imaging();
+		Runner runner = new Runner();
+		NAPD_Contract napd = new NAPD_Contract();
+		corp.addCardToRD(napd);
+		
+		napd.stealAgenda(corp, runner, "researchAndDevelopment");
+		assertTrue(corp.isRDEmpty());
+	}
+	
+	@Test
+	public void testRemoveFromArchivesAfterSteal() {
+		Cerebral_Imaging corp = new Cerebral_Imaging();
+		Runner runner = new Runner();
+		NAPD_Contract napd = new NAPD_Contract();
+		corp.addCardToArchives(napd);
+		
+		napd.stealAgenda(corp, runner, "Archives");
+		assertEquals(0, corp.getSizeArchives());
+	}
+	
+	@Test
+	public void testRemoveFromHQAfterSteal() {
+		Cerebral_Imaging corp = new Cerebral_Imaging();
+		Runner runner = new Runner();
+		NAPD_Contract napd = new NAPD_Contract();
+		corp.addCardToHQ(napd);
+		
+		napd.stealAgenda(corp, runner, "HQ");
+		assertEquals(0, corp.getSizeHQ());
 	}
 
 }
