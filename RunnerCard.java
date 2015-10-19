874222202b6d00b0be58fbca0d323520aacc2686
DSV-Mash <rania.95705mash@gmail.com>
2015-10-19 15:27:45 +0200

    Update RunnerCard.java

--- RunnerCard.java
+++ RunnerCard.java
@@ -5,10 +5,10 @@ public class RunnerCard extends Card{
 	
 	
 	public RunnerCard(String title, String typecode, String subTypeCode, String text, int cost,
-			String faction, String factionCode, String factionletter, String factionCost,
+			 String factionCode, String factionCost,
 			boolean uniqueness) {
 	
-		super(title, typecode, subTypeCode, text, cost, faction, factionCode, factionletter, factionCost,
+		super(title, typecode, subTypeCode, text, cost, factionCode, factionCost,
 				uniqueness);
 		
 		
