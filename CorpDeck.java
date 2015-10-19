338549647d0680c2cb15c5bd257bfab8b43a93e0
DSV-Destel <kevin.niklasson@gmail.com>
2015-10-19 15:37:36 +0200

    Deck fixes
    Man kan nu lägga kort i kortlekar med gson. Lite småjobbig syntax, men
    det funkar seamless.

--- CorpDeck.java
+++ CorpDeck.java
@@ -9,8 +9,16 @@ public class CorpDeck extends Deck {
 		side = "corp";
 	}
 	
-	public void addCard(Card card){
-		getDeckOfCards().add(card);
+	public void addCard(Card[] card, int idx){
+		if(card[idx].getFactionCode().equals("haas-bioroid") ||
+			card[idx].getFactionCode().equals("jinteki") ||
+			card[idx].getFactionCode().equals("nbn") ||
+			card[idx].getFactionCode().equals("weyland") ||
+			card[idx].getFactionCode().equals("neutral")){
+				getDeckOfCards().add(card[0]);			
+			}else{
+				throw new IllegalArgumentException("Not a Corp card!");
+			}
 	}
 
 	
