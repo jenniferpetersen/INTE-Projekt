6769f2edda57b63fc67cb807620af236c0a606cd
Jennifer Petersen <jenniferpetersen089@gmail.com>
2015-10-22 09:56:36 +0200

    100% täckning på AdonisCampaign, metod för att trasha kortet

--- AdonisCampaignTest.java
+++ AdonisCampaignTest.java
@@ -2,19 +2,54 @@ package netrunner;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class AdonisCampaignTest {
 
 	@Test
-	public void testInstallCard() {
+	public void testInstallCardNewRemote() {
 		Corporation corp = new Cerebral_Imaging();
 		AdonisCampaign ac = new AdonisCampaign();
 		corp.addCardToHQ(ac);
 		
 		ac.installCard(corp);
 		assertFalse(corp.isCardInHQ(ac));
-		//Hur testa att kortet har installerats i en remote? Skapa en remote, skicka med och installera kortet där?
+		assertEquals(1, corp.getNoOfRemoteServers());
+	}
+	
+	@Test
+	public void testInstallCardExistingRemote() {
+		Corporation corp = new Cerebral_Imaging();
+		AdonisCampaign ac = new AdonisCampaign();
+		corp.addCardToHQ(ac);
+		ArrayList<Card> remote = corp.createRemoteServer();
+		
+		ac.installCard(corp, remote);
+		assertFalse(corp.isCardInHQ(ac));
+		assertEquals(1, corp.getNoOfRemoteServers());
 	}
 
+	@Test
+	public void testTrashCard() {
+		Runner runner = new Runner();
+		Corporation corp = new Cerebral_Imaging();
+		AdonisCampaign ac = new AdonisCampaign();
+		corp.addCardToHQ(ac);
+
+		ac.trashCard(runner, corp);
+		assertEquals(2, runner.getCredits());
+		assertFalse(corp.isCardInHQ(ac));
+	}
+	@Test (expected = java.lang.IllegalArgumentException.class)
+	public void testTrashCardLowCredits() {
+		Runner runner = new Runner();
+		Corporation corp = new Cerebral_Imaging();
+		AdonisCampaign ac = new AdonisCampaign();
+		corp.addCardToHQ(ac);
+		runner.loseCredits(3);
+
+		ac.trashCard(runner, corp);
+	}
 }
